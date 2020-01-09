67ab4ca57bb4aa53e1a9e427ddcec9cd63c18b47
Actcheng <annacheng310@gmail.com>
2020-01-09 23:08:26 +0900

    Modified default settings of plot_size_dist

--- analysis_tool/point_size_dist.py
+++ analysis_tool/point_size_dist.py
@@ -27,30 +27,43 @@ class PointSizeDist(GroupData):
         
     def set_info(self,info,by='Site name'):
         super().set_info(info,by)
-        self._bin_centers = {key:self._info[key]['data'].get_bin_centers() for key in self._keys}
-        self._bin_num = {key:self._info[key]['data'].get_bin_num() for key in self._keys} 
+        self._bin_centers = {key:self._info[key]['data'].get_bin_centers() 
+                                 if 'total' in self._info[key] and self._info[key]['total']
+                                 else self._info[key]['data'].get_type_info()[key].get_bin_centers()
+                                 for key in self._keys 
+                                 }
+        self._bin_num = {key:self._info[key]['data'].get_bin_num()
+                            if 'total' in self._info[key] and self._info[key]['total']
+                            else self._info[key]['data'].get_type_info()[key].get_bin_num()
+                            for key in self._keys} 
         self._size_col = {key: ['{}.{}'.format(self._info[key]['prefix'],i+1) for i in range(self._bin_num[key])] for key in self._keys}
+
     def get_bin_centers(self):
         return self._bin_centers
 
     def plot_size_dist(self,
-                    sites=['SEDE_BOKER'],prefix='prefix',
+                    sites=[],prefix='prefix',
                     xscale='log',
                     xlabel='Diameter (${\mu}$m)',
-                    ylabel='dV/dlnr',**kwargs):
+                    ylabel='dV/dlnr',
+                    count_key='',
+                    **kwargs):
 
-        if not sites: sites = self._groupnames
+        if len(sites)==0: sites = self._groupnames
         ax_settings = {'xscale': xscale, 'xlabel':xlabel,'ylabel':ylabel}
-
+        
         for i,site in enumerate(sites):
+            counts = 0
             fig,ax = plt.subplots()
             for key in self._keys:
                 centers = self._bin_centers[key]
-                counts = len(self._groupbys[key].get_group(site))
-                data =  list(self._groupbys[key].get_group(site).mean()[self._size_col[key]])
+                group = self._groupbys[key].get_group(site).dropna()
+                if key == count_key: 
+                    counts = len(group)
+                data =  list(group.mean()[self._size_col[key]])
                 ax.plot(centers,data,label=key,**self._styles[key])
 
-            ax_settings['title'] = f'{site}: (N={counts})'
+            ax_settings['title'] = f'{site}: (N={counts})' if counts>0 else site
             ax_settings['save_suffix'] = site
             ax_set(ax,**ax_settings,**kwargs)
 

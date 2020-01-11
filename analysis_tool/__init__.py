8179d57dcf9f4204fdcaa059f57d1f3c7a4f4d2b
Actcheng <annacheng310@gmail.com>
2020-01-11 13:54:33 +0900

    Add comments

--- analysis_tool/__init__.py
+++ analysis_tool/__init__.py
@@ -1,23 +1,45 @@
+## For import
 import sys
 import os
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 sys.path.append(os.path.dirname(CURRENT_DIR)+'/analysis_tool')
 
+## Utils
 import analysis_utils
 import size_utils
+import grads_wrapper
 
+## Point data
 from improve_data import ImproveData
 from nas_data import NasData
 from aeronet_data import AeronetData
 from eusaar_data import EusaarData
 from model_point_data import ModelPointData
 
-# Size
+## Size data
 from type_size_info import TypeSizeInfo
 from model_point_size_data import ModelPointSizeData
 
-# Compare model and obs
+## Compare model and obs
 from point_avg import PointAvg
 from point_hist import PointHist
 from point_size_dist import PointSizeDist
+
+### Abstract classes 
+##
+## base_data  : Parent class of all other classes 
+##
+## group_data : 
+##  - To group time series data
+##  - Parent class of point_hist, point_size_hist 
+## 
+## point_data : 
+##  - To store point data
+##  - Parent class of nas_data, improve_data, aeronet_data, eusaar_data, model_point_data
+##
+## type_size_info:
+##  - To store size information (bin number, dry bin centers) 
+##  - Parent class of aeronet_data, eusaar_data, model_point_size_data
+##
+##################################################################

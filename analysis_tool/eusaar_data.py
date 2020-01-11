d8198b97fbbc48145c324871bf4d7820ab53e25f
Actcheng <annacheng310@gmail.com>
2020-01-11 13:55:15 +0900

    Add `read_size_dist` and other helpers

--- analysis_tool/eusaar_data.py
+++ analysis_tool/eusaar_data.py
@@ -1,3 +1,4 @@
+from type_size_info import TypeSizeInfo
 from point_data import PointData
 from analysis_utils import *
 
@@ -6,10 +7,28 @@ import numpy as np
 import os
 import datetime
 
-class EusaarData(PointData):
+class EusaarData(PointData,TypeSizeInfo):
     def __init__(self):
         PointData.__init__(self)
+        TypeSizeInfo.__init__(self,'EUSAAR','eu','bin',var_type='dns')
         self._code_dict = {}
+        self._size_data = pd.DataFrame
+        self._percentiles = []
+
+    def get_code_dict(self):
+        return self._code_dict
+
+    def get_percentiles(self):
+        return self._percentiles
+
+    def get_size_data(self,percentiles=[],site=''):
+        """ Return data with the specified percentiles and sites"""
+        percentiles = percentiles or self._percentiles
+        df = self._size_data[self._size_data['Percentile'].isin(percentiles)]
+        return df if len(site)==0 else df[df['Site name']==site]
+
+    def set_size_to_all(self,**kwargs):
+        self._all_data = self.get_size_data(**kwargs)
 
     def read_site_info(self,filename):
         """ Required columns: ['Site name','Site code','Latitude','Longitude']"""
@@ -18,9 +37,6 @@ class EusaarData(PointData):
                                .set_index(['Site code'])
                                .to_dict()['Site name'])
         self._site_info = self._site_info.set_index('Site name')
-    
-    def get_code_dict(self):
-        return self._code_dict
 
     def read_time_series(self,filedir,
                         options=['N30','N50','N100','N250','gd'],
@@ -63,13 +79,56 @@ class EusaarData(PointData):
         self._all_data = all_data
         return         
 
-    def read_size_dist(self,file_list,size_filename):
+    def read_size_dist(self,filedir,size_filename,
+                       required=['TOT','BOTH'],
+                       percentiles=[5,15,50,83,95]):
+
         """Read size distributions at different sites"""
         # Read diameters
+        infile = open(size_filename)
+        data = infile.read().split()
+        infile.close()
+        self.set_bin_num(len(data))
+        self.set_bin_centers([float(d)/1e3 for d in data])
+
         # Read sizes and label with percentiles (5,15,50,83 and 95)
+        self._percentiles=percentiles
+        filelist = get_filelist_from_dir(filedir,required=required)
+        site_codes = list(get_sites_from_filenames(filelist))
+        size_data = pd.DataFrame()
 
-        pass
+        print('Number of sites: ', len(site_codes))
 
-    def get_percentile_data(self,percentile=50):
-        """ Return data with the specified percentile """
-        pass
+        for i,site in enumerate(site_codes):
+            site_name = self._code_dict[site] if site in self._code_dict else site
+            
+            site_info = pd.DataFrame(percentiles,columns=['Percentile'])
+            site_info['Site name'] = site_name
+            site_info = site_info[['Site name','Percentile']]
+
+            site_filelist =  get_filelist(filelist,required=[site])
+            filename = site_filelist[0]
+            if len(site_filelist) > 1: 
+                print(f'More than one file for site: {site}! ')
+                print(f'Only the first file is used: {site_filelist[0]}')
+
+            infile = open(os.path.join(filedir,filename))
+            data = [[float(d) for d in row.split()] for row in infile.readlines()]
+            infile.close()
+
+            if len(data) != len(percentiles):
+                print(f'Inconsistent length of data({len(data)}) & input percentiles({len(percentiles)})!')
+                return
+
+            site_df = pd.DataFrame(data,columns=self.get_varlist()).replace(-1,np.nan)
+            site_df = pd.concat([site_info,site_df],axis=1)       
+            
+            size_data = pd.concat([size_data,site_df])
+
+            print(f'Finished reading {i+1}/{len(site_codes)}: {site_name} ({site})')
+
+        self._size_data = size_data
+
+        return 
+
+

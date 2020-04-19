e1a1fe149ad175a18e6db4295635c23212e3417f
Actcheng <annacheng310@gmail.com>
2020-04-20 04:01:57 +0900

    GridData: get_values_region (all values)

--- analysis_tool/grid_data.py
+++ analysis_tool/grid_data.py
@@ -1,5 +1,6 @@
 from base_data import Data
 from grid_info import GridInfo
+import pandas as pd
 
 class GridData(Data,GridInfo):
     def __init__(self,info=None,**kwargs):
@@ -9,4 +10,20 @@ class GridData(Data,GridInfo):
 
     def to_df(self,var_name):
         pass
+
+    def get_values_region(self,ranges=None):
+        """
+        Return a list of tuples of values at each grid point within the specified region
+        
+        Variables:
+        - ranges: Dict which specifies the ranges in each dim
+          e.g. {'lons':[0,100], 'lats':[0,60]}
+
+        """
+        if not ranges:
+            values = {key: self._data[key].flatten() for key in self._data}
+            return pd.DataFrame(values,columns=self._data.keys())
+        for key in ranges:
+            print(key)
+            print(self._grid[key])
 

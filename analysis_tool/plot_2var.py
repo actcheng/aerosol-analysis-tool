5aa30408aabceb0e5a43996236478d9a843c0894
Actcheng <annacheng310@gmail.com>
2020-05-08 00:59:23 +0900

    Added `plot_hist`. `plot_mean` at given axis

--- analysis_tool/plot_2var.py
+++ analysis_tool/plot_2var.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from analysis_utils import ax_set
+import numpy as np
+import matplotlib.colors as mcolors
 
 class Plot2Var():
 
@@ -25,19 +28,33 @@ class Plot2Var():
         return self._yname
 
     ### Plot means & sd of data, grouped by x intervals
-    def plot_mean(self,intervals,plot_std=True,color='k',marker='o'):
+    def plot_mean(self,intervals,plot_std=True,color='k',marker='o',ax=None,label=None,**kwargs):
         groupby = self.data.groupby(pd.cut(self.data[self.xname],intervals))
         mean = groupby.mean()
         x = pd.IntervalIndex(mean.index).mid
         y = mean[self.yname]
         
-        plt.scatter(x,y,color=color,marker=marker)
+        if not ax: fig, ax = plt.subplots()
+
+        ax.scatter(x,y,color=color,marker=marker,label=label)
         if plot_std:
             yerr = groupby.std()[self.yname]
-            plt.errorbar(x,y,yerr=yerr,ls='none',color=color)
+            ax.errorbar(x,y,yerr=yerr,ls='none',color=color)
         
+        ax_set(ax,**kwargs)
         return 
 
-    def plot_hist(self,interval_dict):
-        pass
+    def plot_hist(self,xedges,yedges,ax=None,bounds=None,density=True,cmap='Reds',colorbar=True,**kwargs):
+
+        if not ax: fig, ax = plt.subplots()
+        norm = mcolors.BoundaryNorm(bounds, ncolors=256) if bounds else None
+
+        n = len(self.data[self.xname])
+        weights = np.ones(n)/n if density else np.ones(n)
+ 
+        pcm = ax.hist2d(self.data[self.xname],self.data[self.yname],bins=[xedges,yedges],weights=weights,cmap=cmap,norm=norm)
+        if colorbar: plt.colorbar(pcm[3],ax=ax,extend='both')
+
+        ax_set(ax,legend=False,**kwargs)
+        return pcm
 
